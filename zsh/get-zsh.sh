d0739ba2015284452637861aa0cc7f2dd9da2c11
Ethan He <yfhe@ucdavis.edu>
2020-09-21 19:49:15 -0700

    zsh config

--- zsh/get-zsh.sh
+++ zsh/get-zsh.sh
@@ -4,13 +4,14 @@ sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # autojump
 git clone git://github.com/wting/autojump.git
-python ./autojump/install.py
+cd autojump
+python ./install.py
 sudo apt install autojump   # just to make sure
+cd ..
 rm autojump -rf
 # zsh syntax highlisgting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-# zsh autosuggestions
-# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 
+mv ./zsh-syntax-highlighting ~/.zsh-scripts
 # my version of agnoster theme
 cp ./agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 cp ./zshrc ~/.zshrc
