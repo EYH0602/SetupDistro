d2d2f33790df25246598e2d96c3165f191b32b97
Ethan He <yfhe@ucdavis.edu>
2021-02-21 02:39:33 -0800

    add linux59 host module

--- setpy-arch.sh
+++ setpy-arch.sh
@@ -6,12 +6,13 @@ sudo pacman -S yay binutils make gcc pkg-config fakeroot
 
 # get basic needs
 yay -S vim dmd python3-pip python ghc cabal-install texlive-most \
-    visual-studio-code-bin typora virtualbox dkms neofetch sublime-text appmenu-gtk-module filezilla \
+    visual-studio-code-bin typora  neofetch sublime-text appmenu-gtk-module filezilla \
     discord onedrive-abraunegg signal slack-desktop wps-office trash-cli \
     ttf-firacode nerd-fonts-fira-code lightly-git papirus-icon-theme papirus-filezilla-themes \
     pulse-secure webkitgtk \
     pdfgrep translate-shell \
-    msi-perkeyrgb
+    msi-perkeyrgb \
+    virtualbox dkms linux59-virtualbox-host-modules
 
 
 # active virtualbox
