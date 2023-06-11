c1c7e832a207cf645dd2aba346639fba5d6d3a60
Yifeng He <yfhe@ucdavis.edu>
2023-06-10 23:06:21 -0700

    fix: use https to clone at init

--- get-zsh.sh
+++ get-zsh.sh
@@ -2,7 +2,7 @@
 # install zsh,  oh-my-zsh, and the extensions
 sudo apt install zsh
 # autojump
-git clone git://github.com/wting/autojump.git
+git clone https://github.com/wting/autojump.git
 cd autojump
 python ./install.py
 sudo apt install autojump   # just to make sure
